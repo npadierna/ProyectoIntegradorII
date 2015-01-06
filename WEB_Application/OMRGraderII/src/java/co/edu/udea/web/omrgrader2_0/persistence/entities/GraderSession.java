6b3116633856551769022d09d7ec62650d2a5f31
Proyecto Integrador <npadierna@gmail.com>
2015-01-06 17:35:34 -0500

    Deleting unuseful TODO tasks.

--- WEB_Application/OMRGraderII/src/java/co/edu/udea/web/omrgrader2_0/persistence/entities/GraderSession.java
+++ WEB_Application/OMRGraderII/src/java/co/edu/udea/web/omrgrader2_0/persistence/entities/GraderSession.java
@@ -154,7 +154,6 @@ public class GraderSession implements IEntity, Serializable {
 
     @Override()
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof GraderSession)) {
 
             return (false);
